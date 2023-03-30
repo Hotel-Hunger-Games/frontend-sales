b8ef4f7660f6809283d1496a709b6e92746cf9c5
Dawid Olejnik <dolejnik@fream.pl>
2023-03-30 22:56:26 +0200

    epic listing

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const eventsContainer = document.querySelector('.stays-grid');
-for (let i=0; i<10; i++) {
-    eventsContainer.innerHTML += `
+fetch('http://localhost:8080/api/auction').then(resp => resp.json()).then(data => {
+   data.forEach(auction => {
+       const reservationStartDate = new Date(auction.stay.reservationStartDate*1000).toISOString().split('T')[0];
+       const reservationEndDate = new Date(auction.stay.reservationStartDate*1000).toISOString().split('T')[0];
+       const auctionStartDate = new Date(auction.startDate*1000).toISOString().split('.')[0].replace('T', ' ');
+       const auctionEndDate = new Date(auction.endDate*1000).toISOString().split('.')[0].replace('T', ' ');
+       const roomName = auction.stay.roomDto.name;
+       const button = auction.auctionStatus === 'FINISHED'
+           ? '<a href="auction.html?id=${auction.id}"><button class="button-53" role="button">Licytacja</button></a>'
+           : '<button class="button-53 disabled" role="button" disabled>Licytacja zakończona</button>';
+       const accommodationCapacity = auction.stay.roomDto.accommodationCapacity;
+       // console.log(auction);
+       const price = auction.actualPrice;
+       eventsContainer.innerHTML += `
             <article class="stays-grid-item">
               <div>
-                <h2>Pokój ${i + 1}</h2>
-                <h4>${i + 1} VII 2023 - ${i + 8} VII 2023</h4>
+                <h2>${roomName}</h2>
+                <h3><i>Dla ${accommodationCapacity} osób</i></h3>
+                <h4>Termin: ${reservationStartDate} - ${reservationEndDate}</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mauris nunc, condimentum ultrices mauris vitae, convallis sollicitudin tortor. Integer ante eros, luctus non nibh at, auctor iaculis neque. Suspendisse lobortis pretium libero, blandit fermentum sapien eleifend sit amet. Cras ullamcorper quam mi, eu iaculis metus aliquam non. Phasellus dignissim metus congue nisl suscipit consequat. Integer eros erat, convallis vel enim quis, aliquet rhoncus odio. Curabitur a vestibulum tellus. Donec maximus mi vel augue placerat molestie. .</p>
-                <a href="#"><button class="button-53" role="button">Licytacja</button></a>
+                Aktualna cena: <b>${price} zł</b>
+                ${button}
+                <p><i>początek licytacji: ${auctionStartDate}</i></p>
+                <p><i>koniec licytacji: ${auctionEndDate}</i></p>
               </div>
             </article>
     `;
-}
+   })
+});
 // eventsContainer.innerHTML += `<div/>`;
